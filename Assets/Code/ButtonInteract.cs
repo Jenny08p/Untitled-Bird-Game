b480e6fa395a5b659b888b43e56faa62a3e52812
Jenny <Jenny08p@gmail.com>
2020-04-03 18:05:27 -0400

    Game

---
+++ Assets/Code/ButtonInteract.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonInteract : MonoBehaviour
+{
+    public void StartGame(string level)
+    {
+        Application.LoadLevel("Main");
+    }
+
+    public void MainMenu(string level)
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void Exit(string level)
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    public float speed;
+    public float jumpForce;
+
+    public Text scoreText;
+
+    private int score; 
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        score = 0;
+
+        SetScoreText();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float jumpVelcocity = 2f;
+            rb2d.velocity = Vector2.up * jumpVelcocity; 
+            float moveHorizontal = Input.GetAxis("Horizontal");
+        }
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Spike"))
+        {
+            other.gameObject.SetActive(false);
+            Application.LoadLevel("GameOver");
+        }
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetScoreText();
+        }
+
+        if (other.gameObject.CompareTag("Win"))
+        {
+            Application.LoadLevel("YouWin");
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = ":" + score.ToString();
+    }
+
+}
